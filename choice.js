622f0683db6c52673908fa498d8714fb85cc962b
vignesh2295 <34911519+vignesh2295@users.noreply.github.com>
2017-12-28 16:20:44 +0530

    Add files via upload

---
+++ choice.js
@@ -0,0 +1,56 @@
+﻿function GetDropDown(success,failure)
+ {
+ debugger;
+     $.ajax({
+        url: siteURL + "/_api/web/lists/getbytitle('listname')/fields?$filter=EntityPropertyName eq 'column1' or EntityPropertyName eq 'column2' or EntityPropertyName eq 'column3'",
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: function (data) {
+           //success(data.d.results);
+            success(data.d.results);    
+        },        
+        error: function (data) {
+        debugger;
+            failure(data);
+            
+        }
+        });
+ } 
+/* Function to getting all dropdown items from library success*/
+function DropDownvaluessuccess(Dropdownarray)
+{
+    debugger;    
+     if(Dropdownarray.length>0)
+     {
+        var cln1dd='<option selected value="Select">Select</option>';
+        var cln2dd='<option selected value="Select">Select</option>';
+        var cln3dd='<option selected value="Select">Select</option>';
+             
+            var cln1=Dropdownarray[0].Choices.results;
+            var cln2=Dropdownarray[1].Choices.results;
+            var cln3=Dropdownarray[2].Choices.results;
+            
+            for(var i=0;i<cln1.length;i++)
+            {               
+                cln1dd+='<option value='+i+'>'+cln1[i]+'</option>';
+            }
+            for(var i=0;i<cln2.length;i++)
+            {               
+                cln2dd+='<option value='+i+'>'+cln2[i]+'</option>';               
+            }
+            for(var i=0;i<cln3.length;i++)
+            {
+                cln3dd+='<option value='+i+'>'+cln3[i]+'</option>';
+            }
+            
+            $("#TaxType").empty().append(cln1dd);
+            $("#cln2").empty().append(cln2dd);
+            $("#TaxCountry").empty().append(cln3dd);
+                    
+     }       
+}
+/* Function to getting all dropdown items from library on failure*/ 
+function DropDownvaluesfailure()
+{
+    debugger; 
+}
---
+++ dynamic csv.js
@@ -0,0 +1,24 @@
+﻿function WriteToFile() {
+debugger;
+      var data0 = [
+        ["A","B","C","D"],
+        ["a","b","c","d"]
+      ];
+      var docTitle = "SupplierBusinessClassification.csv";     
+        download_csv(data0,docTitle)            
+}
+ 
+function download_csv(data,docName) {
+    var csv;
+    data.forEach(function(row) {
+        csv += row.join(',');
+        csv += "\n";
+    });
+ 
+    console.log(csv);
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_parent';
+    hiddenElement.download = docName;
+    hiddenElement.click();
+}
